46de473b0f350585111584a15edafce31e7b1520
rajesh <peeyar2000@gmail.com>
2019-07-21 14:10:50 -0700

    cleanup and created packages

--- src/main/java/com/redfin/interview/controller/FoodTruckController.java
+++ src/main/java/com/redfin/interview/controller/FoodTruckController.java
@@ -3,13 +3,18 @@ package com.redfin.interview.controller;
 
 import com.redfin.interview.entity.FoodTruck;
 import com.redfin.interview.service.FoodTruckService;
+
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FoodTruckController {
@@ -21,12 +26,14 @@ public class FoodTruckController {
     }
 
     @GetMapping(path = "/openfoodtrucks")
-    Page<FoodTruck> loadCharactersPage(
-        @PageableDefault(page = 0, size = 50)
-        @SortDefault.SortDefaults({
-            @SortDefault(sort = "truckName", direction = Direction.ASC)
-        }) Pageable pageable) {
-        return foodTruckService.getOpenFoodTruckAtCurrentTime(pageable);
+    ResponseEntity<List<FoodTruck>> loadCharactersPage(
+            @PageableDefault(page = 0, size = 50)
+            @SortDefault.SortDefaults({
+                    @SortDefault(sort = "truckName", direction = Direction.ASC)
+            }) Pageable pageable) {
+        List<FoodTruck> response = foodTruckService
+                .getOpenFoodTruckAtCurrentTime(pageable).getContent();
+        return ResponseEntity.ok().body(response);
     }
 
 }
